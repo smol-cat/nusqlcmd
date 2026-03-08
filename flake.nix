385d0930efbee02e580d85cc85ea06932240c532
smol-cat <ernestasunt@gmail.com>
2026-03-08 22:03:38 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Nusqlcmd";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      version = "0.1.0";
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          nusqlcmd = pkgs.buildGoModule {
+            pname = "nusqlcmd";
+            inherit version;
+            src = ./.;
+            vendorHash = "sha256-r2N6qr1P6ElMyYMnvYwjsc6iGsakp4qWTxZP4eJ2jGw=";
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+            ];
+          };
+        }
+      );
+    };
+}
