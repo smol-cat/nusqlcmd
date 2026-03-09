eed2f9fcd7fd65e23f13bbf4ad92374503225c5d
smol-cat <ernestasunt@gmail.com>
2026-03-09 09:43:14 +0200

    Remove draft table serialization

--- internal/serialization/json_serialization.go
+++ internal/serialization/json_serialization.go
@@ -7,6 +7,25 @@ import (
 	"github.com/smol-cat/nusqlcmd/internal/common"
 )
 
+func scanRow(rows *sql.Rows, colCount int) []string {
+	var rawCols = make([]any, colCount)
+
+	for i := range rawCols {
+		var alloc string
+		rawCols[i] = &alloc
+	}
+
+	err := rows.Scan(rawCols...)
+	common.PanicOnErr(err)
+
+	var cols = make([]string, colCount)
+	for i := range rawCols {
+		cols[i] = *(rawCols[i].(*string))
+	}
+
+	return cols
+}
+
 func SerializeToJson(rows *sql.Rows) string {
 	colNames, err := rows.Columns()
 	common.PanicOnErr(err)
